f66b089c5a3d63a6f8fc420140c2e530195ac63a
Ryan Nauman <ryanjnauman@gmail.com>
2021-02-03 09:01:09 -0500

    archive

---
+++ old/rules.mk
@@ -0,0 +1,12 @@
+SRC += muse.c               # what is this?
+
+# Build Options
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+
+UNICODE_ENABLE = no         # Unicode
+UNICODEMAP_ENABLE = yes
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
